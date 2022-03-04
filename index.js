f38030390c72262075f19cc627152a749aaa4c1d
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2022-03-04 22:09:08 +0300

    feat: percent functional

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const btn = document.querySelector('.btn');
 const buttonAC = document.querySelector('.ac');
 const form = document.querySelector('.form');
 
-const numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '%'];
 const signsList = ['-', '+', 'X', '/'];
 
 let sign = ''; 
 let firstNum = ''; 
 let secondNum = ''; 
+let percentOfNum = null;
 
 let finish  = false;
 
@@ -27,6 +28,19 @@ form.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
     if (secondNum ==='') secondNum = firstNum;
+    if (firstNum.includes('%')) {
+        return result.textContent = 0;
+    }
+    else if (secondNum.includes('%')) {
+        if (sign === '+') {
+            secondNum = secondNum.split('').filter(el => el !== '%').join('') * firstNum / 100;
+        }
+       else if (sign === 'X') {
+            percentOfNum = secondNum.split('').filter(el => el !== '%').join('') * firstNum / 100;
+
+        console.log(percentOfNum)
+       }
+    }
         switch (sign) {
             case "+":
                 firstNum = (+firstNum) + (+secondNum);
@@ -35,11 +49,12 @@ form.addEventListener('submit', (evt) => {
                 firstNum = firstNum - secondNum;
                 break;
             case "X":
-                firstNum = firstNum * secondNum;
+                if (percentOfNum !== null) firstNum = `${percentOfNum}`;
+                else firstNum = firstNum * secondNum;
                 break;
             case "/":
                 if (secondNum === '0') {
-                    result.textContent = 'Error';
+                    result.textContent = 'Err';
                     firstNum = '';
                     secondNum = '';
                     sign = '';
@@ -47,10 +62,24 @@ form.addEventListener('submit', (evt) => {
                 }
                 firstNum = firstNum / secondNum;
                 break;
+                // case "%":
+                //     if (secondNum === '0') firstNum;
+                //     else if (secondNum === '') {
+                //         result.textContent = 'Err';
+                //     firstNum = '';
+                //     secondNum = '';
+                //     sign = '';
+                //     return;
+                //     }
+                //     else {
+                //         secondNum = `${secondNum}%`;
+                //         firstNum = firstNum + secondNum;
+                //     }
+                    // firstNum = firstNum * secondNum / 100;
         }
         finish = true;
         result.textContent = firstNum;
-        console.table(firstNum, secondNum , sign);
+
     
 
 })
@@ -62,18 +91,11 @@ buttons.addEventListener('click', (event) => {
     
     const pressedBtn = event.target.textContent;
 
-    // если нажата клавиша 0-9 или ,
-    if (numbersList.includes(pressedBtn)) {
-        // if (pressedBtn === '.' && firstNum.includes('.')) {
-        //     firstNum += '';
-        //     result.textContent = firstNum;
-        // } else {
-        //     firstNum += pressedBtn;
 
-        //     result.textContent = firstNum;
-        // }
+    if (numbersList.includes(pressedBtn)) {
+        
         if (secondNum ==='' && sign === '') {
-                    if (pressedBtn === '.' && firstNum.includes('.')) {
+            if (pressedBtn === '.' && firstNum.includes('.')) {
             firstNum += '';
             result.textContent = firstNum;
         } else {
@@ -81,58 +103,26 @@ buttons.addEventListener('click', (event) => {
 
             result.textContent = firstNum;
         }
-            // firstNum += pressedBtn;
-            
-            // result.textContent = firstNum;
         }
         else if (firstNum!=='' && secondNum!=='' && finish) {
             secondNum = pressedBtn;
             finish = false;
             result.textContent = secondNum;
         }
+        // else 
         else {
             secondNum += pressedBtn;
             result.textContent = secondNum;
         }
-        console.table(firstNum, secondNum , sign);
+
         return;
     }
 
-     // если нажата  + - / *
+     // если нажата  + - / * 
      if (signsList.includes(pressedBtn)) {
         sign = pressedBtn;
         result.textContent = sign;
-        console.table(firstNum, secondNum , sign);
+
         return;
     }
-
-    // нажата =
-    // if (pressedBtn === '=') {
-    //     if (secondNum ==='') secondNum = firstNum;
-    //     switch (sign) {
-    //         case "+":
-    //             firstNum = (+firstNum) + (+secondNum);
-    //             break;
-    //         case "-":
-    //             firstNum = firstNum - secondNum;
-    //             break;
-    //         case "X":
-    //             firstNum = firstNum * secondNum;
-    //             break;
-    //         case "/":
-    //             if (secondNum === '0') {
-    //                 result.textContent = 'Ошибка';
-    //                 firstNum = '';
-    //                 secondNum = '';
-    //                 sign = '';
-    //                 return;
-    //             }
-    //             firstNum = firstNum / secondNum;
-    //             break;
-    //     }
-    //     finish = true;
-    //     result.textContent = firstNum;
-    //     console.table(firstNum, secondNum , sign);
-    // }
-
 });
