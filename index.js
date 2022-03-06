951e8b6eb3e8c802d170b1a39a34912588dfc493
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2022-03-06 14:46:15 +0300

    refactor: functions and listeners

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const result = document.querySelector('.result_text');
 const buttons = document.querySelector('.buttons');
 const btn = document.querySelector('.btn');
+
 const buttonCE = document.querySelector('.ce');
 const buttonC = document.querySelector('.c');
 const buttonMinusPlus = document.querySelector('.changeSign');
-const buttonBackspace = document.querySelector('.backspace')
+const buttonBackspace = document.querySelector('.backspace');
+const buttonQuadrate = document.querySelector('.quadrate');
+const buttonFraction = document.querySelector('.fraction');
+const buttonRoot = document.querySelector('.root');
+
+
 const form = document.querySelector('.form');
 
 const numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '%'];
-const signsList = ['-', '+', '×', '/', 'x²', '¹⁄ₓ', '√ₓ'];
+const signsList = ['-', '+', '×', '/'];
 
 let sign = ''; 
 let firstNum = ''; 
 let secondNum = ''; 
 let percentOfNum = null;
-
+let changedNum;
 let finish  = false;
-
+let whatIsNumber = null;
 
 // очистить результат
 function clearResult () {
@@ -27,76 +33,98 @@ function clearResult () {
     result.textContent = 0;
 }
 
+// очистить последнее число
 function clearNum () {
-    if (firstNum !== '' && secondNum === '') {
-        firstNum = ''; 
-    }
-    else secondNum = '';
-
-    result.textContent = 0;
+    return result.textContent = 0;
 }
 
-function changeNumberSign () {
-    let newArray = [];
-    if(result.textContent === firstNum ) {
-        if (firstNum > 0) firstNum = `-${firstNum}`
-        else { 
-            newArray = firstNum.split('');
-            newArray.shift()
-            firstNum = newArray.join('')
-}
-        result.textContent = firstNum;
+function actualResult () {
+    if (whatIsNumber === 'firstNum') {
+        if (result.textContent == 0) return firstNum = '';
+        else return firstNum = result.textContent;
+    } 
+    else if (whatIsNumber === 'secondNum') {
+        if (result.textContent == 0) return secondNum = '';
+        else return secondNum = result.textContent;
     }
+}
+
+function changeNumberSign (number) {
+    if (number > 0) return result.textContent = `-${number}`;
     else {
-        if (secondNum > 0) secondNum = `-${secondNum}`;
-        else { 
-            
-            newArray = secondNum.split('');
-            newArray.shift()
-            secondNum = newArray.join('')
+        let positiveNumber = number.split('');
+        positiveNumber.shift();
+        return result.textContent = positiveNumber.join('');
+    } 
 }
 
-        result.textContent = secondNum;
-    }
-}
-
-function backspace () {
-    let backArray = [];
+function backspace (number) {
     if (result.textContent === 0) return;
     else {
-    backArray = result.textContent.split('');
-    backArray.pop();
-    if (result.textContent === firstNum) {
-        console.log('f')
-        if (firstNum.length === 1 || firstNum.length === 0) {
-            firstNum = '';
-            result.textContent = 0;
-        } else {
-            firstNum = backArray.join('');
-            result.textContent = firstNum;
+        let reduceValue = result.textContent.split('');
+        reduceValue.pop();
+        if (number.length === 1 || number.length === 0) {
+            return result.textContent = 0;
         }
-    } else if (firstNum === '') result.textContent = 0;
-    else { 
-        if (secondNum.length === 1) {
-            secondNum = ''
-            result.textContent = 0;
-        } 
-        else if (secondNum === '') result.textContent = 0;
-        else {
-            secondNum = backArray.join('');
-            result.textContent = secondNum;}
-        }
+        else return result.textContent = reduceValue.join('');
     }
 }
+
+function quadrateNumber (number) {
+
+    result.textContent = number * number;
+    
+}
+
+function fractionNumber (number) {
+    result.textContent = 1 / number;
+}
+
+function sqrtNumber (number) {
+    // console.log(Math.sqrt(number))
+    result.textContent = Math.sqrt(number);
+}
+
+buttonC.addEventListener('click', clearResult);
+
+buttonCE.addEventListener('click', () => {
+    clearNum();
+    if (whatIsNumber === 'firstNum') return firstNum = '';
+    else if (whatIsNumber === 'secondNum') return secondNum = '';
+})
+
+buttonMinusPlus.addEventListener('click', () => {
+    if (whatIsNumber === 'firstNum') changeNumberSign(firstNum);
+    else if (whatIsNumber === 'secondNum') changeNumberSign(secondNum);
+    return actualResult();
+})
+
+buttonQuadrate.addEventListener('click', () => {
+    if (whatIsNumber === 'firstNum') quadrateNumber(firstNum);
+    else if (whatIsNumber === 'secondNum') quadrateNumber(secondNum);
+    return actualResult();
+    
+})
+
+buttonBackspace.addEventListener('click', () => {
+    if (whatIsNumber === 'firstNum') backspace(firstNum);
+    else if (whatIsNumber === 'secondNum') backspace(secondNum);
+    return actualResult();
+    // else if (result.textContent === 0 && whatIsNumber === 'firstNum') return firstNum = '';
+    // else if (result.textContent === 0 && whatIsNumber === 'secondNum') return secondNum = '';
+})
 
 buttons.addEventListener('click', (event) => {
     
     if(!event.target.classList.contains('btn')) return;
-    else if (event.target === buttonCE) clearNum();
-    else if (event.target === buttonC) clearResult();
-    else if (event.target === buttonMinusPlus ) changeNumberSign();
-    else if (event.target === buttonBackspace) backspace();
-
+    // else if (event.target === buttonCE) clearNum();
+    // else if (event.target === buttonC) clearResult();
+    // else if (event.target === buttonMinusPlus ) changeNumberSign();
+    // else if (event.target === buttonBackspace) backspace();
+    // else if (event.target === buttonQuadrate) quadrateNumber()
+    else if (event.target === buttonFraction) fractionNumber(firstNum);
+    else if (event.target === buttonRoot) sqrtNumber(firstNum);
+    
     const pressedBtn = event.target.textContent;
 
     if (numbersList.includes(pressedBtn)) {
@@ -107,20 +135,21 @@ buttons.addEventListener('click', (event) => {
             result.textContent = firstNum;
         } else {
             firstNum += pressedBtn;
-
+            whatIsNumber = 'firstNum';
             result.textContent = firstNum;
         }
         }
         else if (firstNum!=='' && secondNum!=='' && finish) {
             if (sign === '') {
-                console.log(sign, 'pusto')
                 firstNum = pressedBtn;
                 result.textContent = firstNum;
+                whatIsNumber = 'firstNum';
             }
             else {
                 secondNum += pressedBtn;
                 console.log('n')
                 result.textContent = secondNum;
+                whatIsNumber = 'secondNum';
                 finish = false;
             }
 
@@ -128,8 +157,9 @@ buttons.addEventListener('click', (event) => {
         else {
             secondNum += pressedBtn;
             result.textContent = secondNum;
+            whatIsNumber = whatIsNumber = 'secondNum';
         }
-        console.log(firstNum, secondNum, sign)
+        console.log(firstNum, secondNum, sign, whatIsNumber)
         return;
     }
 
@@ -137,45 +167,19 @@ buttons.addEventListener('click', (event) => {
      if (signsList.includes(pressedBtn)) {
         sign = pressedBtn;
 
-        if (sign === 'x²' || sign === '¹⁄ₓ' || sign === '√ₓ' || sign === '⌫') {
+        if (sign === '√ₓ') {
             switch(sign) {
-                case 'x²':
-                    firstNum = firstNum * firstNum;
-                    break;
-                case '¹⁄ₓ':
-                    firstNum = 1 / firstNum;
-                    break;
+
+                // case '¹⁄ₓ':
+                //     firstNum = 1 / firstNum;
+                //     break;
                 case '√ₓ':
                     firstNum = Math.sqrt(firstNum);
                     break;
-                // case '⌫':
-                //     let backArray = [];
-
-                //     backArray = result.textContent.split('');
-                //     backArray.pop();
-                //     if (result.textContent === firstNum) {
-                //         if (firstNum.length === 1) {
-                //             firstNum = '';
-                //             result.textContent = 0;
-                //         }
-                //         else {
-                //         firstNum = backArray.join('');
-                //         result.textContent = firstNum;
-                //     }
-                //     }
-                //     else { 
-                //         if (secondNum.length === 1) {
-                //             secondNum = ''
-                //             result.textContent = 0;
-                //         } 
-                //         else {
-                //         secondNum = backArray.join('');
-                //         result.textContent = secondNum;}
-                //     }
-                //     break;
+                
 
             }
-            // if (sign !== '⌫') 
+            whatIsNumber = 'firstNum';
             result.textContent = firstNum;
                     
             finish = true;
@@ -183,13 +187,15 @@ buttons.addEventListener('click', (event) => {
             
         }
         
-        else result.textContent = sign;
+        else 
+        result.textContent = sign;
         finish = true;
         // sign = '';
         return;
         
     }
-    console.log(firstNum, secondNum, sign)
+
+    console.log(firstNum, secondNum, sign, whatIsNumber)
 });
 
 form.addEventListener('submit', (evt) => {
@@ -234,6 +240,7 @@ console.log(percentOfNum)
         sign='';
         secondNum='';
         result.textContent = firstNum;
-        console.log(firstNum, secondNum, sign)
+        whatIsNumber = 'firstNum';
+        console.log(firstNum, secondNum, sign, whatIsNumber)
 })
 
